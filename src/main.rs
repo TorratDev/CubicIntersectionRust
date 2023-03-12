bfa5ae40c430d36aa122b260b624d6854418fd10
Ruben Dario <torratDev@gmail.com>
2023-03-12 18:55:09 +0100

    add api endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use rocket::{get, serde::json::Json};
 use serde_json::json;
-
 fn main() {
     println!("Hello, world!");
 
+    /// First api enpoint
     #[get("/")]
-    fn index() -> Json {
+    fn index() -> Json<serde_json::Value> {
         let data = json!({
             "message": "Hello, world!"
         });
